fc7c01a272ae33699e686a56db76facbf6b874fa
NarrikSynthfox <80410683+NarrikSynthfox@users.noreply.github.com>
2024-05-05 00:31:01 -0400

    Update song.h

--- Encore/include/song/song.h
+++ Encore/include/song/song.h
@@ -19,9 +19,6 @@ enum PartIcon {
 	IconNone
 };
 
-
-
-
 enum SongParts {
 	PartDrums,
 	PartBass,
@@ -113,21 +110,21 @@ public:
     }
 
 	bool midiParsed=false;
-	std::string title;
+	std::string title = "";
 	float titleXOffset = 0;
 	float titleTextWidth = 0;
 	double titleScrollTime = 0.0;
-	std::string artist;
+	std::string artist = "";
 	float artistXOffset = 0;
 	float artistTextWidth = 0;
 	double artistScrollTime = 0.0;
     Texture albumArtBlur;
 	Texture albumArt;
-
+	std::string album = "";
 	int length = 0;
 	
-	std::vector<BPM> bpms;
-	std::vector<TimeSig> timesigs;
+	std::vector<BPM> bpms{};
+	std::vector<TimeSig> timesigs{};
 
 	double music_start=0.0;
 	double end=0.0;
@@ -137,9 +134,15 @@ public:
 
 	std::vector<std::pair<double, bool>> beatLines; //double time, bool downbeat
 
-	std::vector<std::pair<std::string, int>> stemsPath{ {"",0},{"",1},{"",2},{"",3},{"",4} };
+	std::vector<std::pair<std::string, int>> stemsPath{};
 
 	std::filesystem::path midiPath = "";
+
+	int releaseYear = 0;
+	std::string loadingPhrase = "";
+	std::vector<std::string> charters{};
+
+
 
 	void LoadSong(std::filesystem::path jsonPath) 
 	{
@@ -156,127 +159,132 @@ public:
 
 		if (document.IsObject()) 
 		{
-			if (document.HasMember("title") && document["title"].IsString()) 
-			{
-				title = document["title"].GetString();
-			}
-
-			if (document.HasMember("artist") && document["artist"].IsString()) 
-			{
-				artist = document["artist"].GetString();
-			}
-
-			if (document.HasMember("length") && document["length"].IsInt()) 
-			{
-				length = document["length"].GetInt();
-			}
-
-			if (document.HasMember("sid") && document["sid"].IsString()) 
-			{
-				partIcons[0] = iconFromString(document["sid"].GetString());
-			}
-
-			if (document.HasMember("sib") && document["sib"].IsString()) 
-			{
-				partIcons[1] = iconFromString(document["sib"].GetString());
-			}
-
-			if (document.HasMember("sig") && document["sig"].IsString()) 
-			{
-				partIcons[2] = iconFromString(document["sig"].GetString());
-			}
-
-			if (document.HasMember("siv") && document["siv"].IsString()) 
-			{
-				partIcons[3] = iconFromString(document["siv"].GetString());
-			}
-
-			if (document.HasMember("diff") && document["diff"].IsObject()) 
-			{
-				for (rapidjson::Value::ConstMemberIterator itr = document["diff"].MemberBegin(); itr != document["diff"].MemberEnd(); ++itr) 
+			for (auto& item : document.GetObject()) {
+				if (item.name == "title" && item.value.IsString())
+					title = item.value.GetString();
+				if (item.name == "artist" && item.value.IsString())
+					artist = item.value.GetString();
+				if (item.name == "album" && item.value.IsString())
+					album = item.value.GetString();
+				if (item.name == "length" && item.value.IsInt())
+					length = item.value.GetInt();
+				if (item.name == "release_year" && item.value.IsInt())
+					releaseYear = item.value.GetInt();
+				if (item.name == "loading_phrase" && item.value.IsString())
+					loadingPhrase = item.value.GetString();
+				if ((item.name=="sid" || item.name=="icon_drums") && item.value.IsString())
+					partIcons[0] = iconFromString(item.value.GetString());
+				if ((item.name == "sib" || item.name == "icon_bass") && item.value.IsString())
+					partIcons[1] = iconFromString(item.value.GetString());
+				if ((item.name == "sig" || item.name == "icon_guitar") && item.value.IsString())
+					partIcons[2] = iconFromString(item.value.GetString());
+				if ((item.name == "siv" || item.name == "icon_vocals") && item.value.IsString())
+					partIcons[3] = iconFromString(item.value.GetString());
+				if (item.name == "midi" && item.value.IsString())
+					midiPath = jsonPath.parent_path() / item.value.GetString();
+				if (item.name == "art" && item.value.IsString()) {
+					std::string artPath = (jsonPath.parent_path() / item.value.GetString()).string();
+					Image albumImage = LoadImage(artPath.c_str());
+					if (albumImage.height > 512) {
+						ImageResize(&albumImage, 512, 512);
+					}
+					albumArt = LoadTextureFromImage(albumImage);
+					SetTextureFilter(albumArt, TEXTURE_FILTER_ANISOTROPIC_16X);
+					ImageBlurGaussian(&albumImage, 10);
+					albumArtBlur = LoadTextureFromImage(albumImage);
+					UnloadImage(albumImage);
+				}
+				if (item.name=="diff" && item.value.IsObject())
 				{
-					std::string part = std::string(itr->name.GetString());
-					if (part == "ds")
+					for (auto& diff:item.value.GetObject())
 					{
-						parts[0]->diff = itr->value.GetInt();
-					}
-
-					else if (part == "ba")
-					{
-						parts[1]->diff = itr->value.GetInt();
-					}
-
-					else if (part == "gr")
-					{
-						parts[2]->diff = itr->value.GetInt();
-					}
-
-					else if (part == "vl")
-					{
-						parts[3]->diff = itr->value.GetInt();
-					}
-
-					else if (part == "pd")
-					{
-						parts[4]->diff = itr->value.GetInt();
-					}
-
-					else if (part == "pb")
-					{
-						parts[5]->diff = itr->value.GetInt();
-					}
-
-					else if (part == "pg")
-					{
-						parts[6]->diff = itr->value.GetInt();
+						std::string part = std::string(diff.name.GetString());
+						if (item.value.IsInt()) {
+							if (part == "ds" || part == "drums")
+							{
+								parts[0]->diff = diff.value.GetInt();
+							}
+							else if (part == "ba" || part == "bass")
+							{
+								parts[1]->diff = diff.value.GetInt();
+							}
+							else if (part == "gr" || part == "guitar")
+							{
+								parts[2]->diff = diff.value.GetInt();
+							}
+							else if (part == "vl" || part == "vocals")
+							{
+								parts[3]->diff = diff.value.GetInt();
+							}
+							else if (part == "pd" || part == "plastic_drums")
+							{
+								parts[4]->diff = diff.value.GetInt();
+							}
+							else if (part == "pb" || part == "plastic_bass")
+							{
+								parts[5]->diff = diff.value.GetInt();
+							}
+							else if (part == "pg" || part == "plastic_guitar")
+							{
+								parts[6]->diff = diff.value.GetInt();
+							}
+						}
 					}
 				}
-			}
-
-			if (document.HasMember("midi") && document["midi"].IsString()) 
-			{
-				midiPath = jsonPath.parent_path() / document["midi"].GetString();
-			}
-
-			if (document.HasMember("stems") && document["stems"].IsObject()) 
-			{
-				for (rapidjson::Value::ConstMemberIterator itr = document["stems"].MemberBegin(); itr != document["stems"].MemberEnd(); ++itr) 
+				if (item.name=="stems" && item.value.IsObject())
 				{
-					std::string stem = std::string(itr->name.GetString());
-
-					if (std::filesystem::exists(jsonPath.parent_path() / itr->value.GetString())) 
+					for (auto &path : item.value.GetObject())
 					{
-						if (stem == "drums")
-							stemsPath[0].first = (jsonPath.parent_path() / itr->value.GetString()).string();
+						std::string stem = std::string(path.name.GetString());
+						if (path.value.IsString()) {
+							if (std::filesystem::exists(jsonPath.parent_path() / path.value.GetString()))
+							{
+								if (stem == "drums")
+									stemsPath.push_back({ (jsonPath.parent_path() / path.value.GetString()).string(),0 });
 
-						else if (stem == "bass")
-							stemsPath[1].first = (jsonPath.parent_path() / itr->value.GetString()).string();
+								else if (stem == "bass")
+									stemsPath.push_back({ (jsonPath.parent_path() / path.value.GetString()).string(),1 });
 
-						else if (stem == "lead")
-							stemsPath[2].first = (jsonPath.parent_path() / itr->value.GetString()).string();
+								else if (stem == "lead")
+									stemsPath.push_back({ (jsonPath.parent_path() / path.value.GetString()).string(),2 });
 
-						else if (stem == "vocals")
-							stemsPath[3].first = (jsonPath.parent_path() / itr->value.GetString()).string();
+								else if (stem == "vocals")
+									stemsPath.push_back({ (jsonPath.parent_path() / path.value.GetString()).string(),3 });
 
-						else if (stem == "backing")
-							stemsPath[4].first = (jsonPath.parent_path() / itr->value.GetString()).string();
+								else if (stem == "backing")
+									stemsPath.push_back({ (jsonPath.parent_path() / path.value.GetString()).string(),4 });
+							}
+						}
+						else if (path.value.IsArray()) {
+							for (auto& path : path.value.GetArray()) {
+								if (std::filesystem::exists(jsonPath.parent_path() / path.GetString()))
+								{
+									if (stem == "drums")
+										stemsPath.push_back({ (jsonPath.parent_path() / path.GetString()).string(),0 });
+
+									else if (stem == "bass")
+										stemsPath.push_back({ (jsonPath.parent_path() / path.GetString()).string(),1 });
+
+									else if (stem == "lead")
+										stemsPath.push_back({ (jsonPath.parent_path() / path.GetString()).string(),2 });
+
+									else if (stem == "vocals")
+										stemsPath.push_back({ (jsonPath.parent_path() / path.GetString()).string(),3 });
+
+									else if (stem == "backing")
+										stemsPath.push_back({ (jsonPath.parent_path() / path.GetString()).string(),4 });
+								}
+							}
+						}
 					}
-				}					
-			}
-
-			if (document.HasMember("art") && document["art"].IsString()) 
-			{
-				std::string artPath = (jsonPath.parent_path() / document["art"].GetString()).string();
-				Image albumImage = LoadImage(artPath.c_str());
-
-				if (albumImage.height > 512) {
-                    ImageResize(&albumImage, 512, 512);
-                }
-				albumArt = LoadTextureFromImage(albumImage);
-                SetTextureFilter(albumArt, TEXTURE_FILTER_ANISOTROPIC_16X);
-                ImageBlurGaussian(&albumImage, 10);
-                albumArtBlur = LoadTextureFromImage(albumImage);
-				UnloadImage(albumImage);
+				}
+				if (item.name == "charters" && item.value.IsObject()) {
+					for(auto& charter:item.value.GetObject()){
+						if (charter.value.IsString()) {
+							charters.push_back(charter.value.GetString());
+						}
+					}
+				}
 			}
 		}
 		ifs.close();
