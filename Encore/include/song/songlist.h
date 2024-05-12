c38c7d83eba0b2b99013d789900a4b9ff06428e3
NarrikSynthfox <80410683+NarrikSynthfox@users.noreply.github.com>
2024-05-12 15:19:55 -0400

    basic sorting

--- Encore/include/song/songlist.h
+++ Encore/include/song/songlist.h
@@ -3,15 +3,59 @@
 #include <vector>
 #include <filesystem>
 #include <thread>
-
+#include <algorithm>
 class SongList
 {
 public:
+    static bool sortArtist(const Song& a, const Song& b) {
+        return a.artist < b.artist;
+    }
+    static bool sortTitle(const Song& a, const Song& b) {
+        return a.title < b.title;
+    }
+    static bool sortLen(const Song& a, const Song& b) {
+        return a.length < b.length;
+    }
     std::vector<Song> songs;
     int songCount = 0;
     int directoryCount  = 0;
     int badSongCount = 0;
-
+    //0 - title
+    //1 - artist
+    //2 - length
+    void sortList(int sortType) {
+        switch (sortType) {
+            case (0):
+                std::sort(songs.begin(), songs.end(), sortTitle);
+                break;
+            case (1):
+                std::sort(songs.begin(), songs.end(), sortArtist);
+                break;
+            case (2):
+                std::sort(songs.begin(), songs.end(), sortLen);
+                break;
+        }
+    }
+    void sortList(int sortType,int& selectedSong) {
+        Song curSong = songs[selectedSong];
+        selectedSong = 0;
+        switch (sortType) {
+        case (0):
+            std::sort(songs.begin(), songs.end(), sortTitle);
+            break;
+        case (1):
+            std::sort(songs.begin(), songs.end(), sortArtist);
+            break;
+        case (2):
+            std::sort(songs.begin(), songs.end(), sortLen);
+            break;
+        }
+        for (int i = 0; i < songs.size();i++) {
+            if (songs[i].artist == curSong.artist && songs[i].title == curSong.title) {
+                selectedSong = i;
+            }
+        }
+    }
     SongList LoadSongs(const std::vector<std::filesystem::path>& songsFolder)
     {
         SongList list;
@@ -30,6 +74,7 @@ public:
                 }
             }
         }
+        list.sortList(0);
         return list;
     }
 };
