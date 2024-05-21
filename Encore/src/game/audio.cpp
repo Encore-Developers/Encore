3e26be3f4f3b40febe2e6aa65c79b9a1cc5e8783
kimicake <alyssakeik@gmail.com>
2024-05-21 01:32:12 -0700

    minor changes to the audio manager

--- Encore/src/game/audio.cpp
+++ Encore/src/game/audio.cpp
@@ -3,13 +3,10 @@
 #include "GLFW/glfw3.h"
 
 #ifdef WIN32
-#define GLFW_EXPOSE_NATIVE_WIN32
+    #define GLFW_EXPOSE_NATIVE_WIN32
 #elif __linux__
-
-#include <X11/Xlib.h>
-
-#define GLFW_EXPOSE_NATIVE_X11
-
+    #include <X11/Xlib.h>
+    #define GLFW_EXPOSE_NATIVE_X11
 #endif
 
 #include "GLFW/glfw3native.h"
@@ -17,66 +14,101 @@
 #include <filesystem>
 #include <iostream>
 
+// Error checking macro
+#define CHECK_BASS_ERROR() { \
+    int errorCode = BASS_ErrorGetCode(); \
+    if (errorCode != BASS_OK) { \
+        std::cerr << "BASS error " << errorCode << " at line " << __LINE__ << std::endl; \
+        return false; \
+    } \
+}
+
 bool AudioManager::Init() {
 #ifdef WIN32
-	return BASS_Init(-1, 44100, 0, glfwGetWin32Window(glfwGetCurrentContext()), NULL);
+    if (!BASS_Init(-1, 44100, 0, glfwGetWin32Window(glfwGetCurrentContext()), NULL)) {
+        CHECK_BASS_ERROR();
+    }
 #elif __linux__
-	return BASS_Init(-1, 44100, 0, 0, NULL);
+    if (!BASS_Init(-1, 44100, 0, 0, NULL)) {
+        CHECK_BASS_ERROR();
+    }
 #endif
-};
+    return true;
+}
 
-void AudioManager::loadStreams(std::vector<std::pair<std::string,int>>& paths) {
+void AudioManager::loadStreams(std::vector<std::pair<std::string, int>>& paths) {
     int streams = 0;
-	for (auto& path : paths) {
-		AudioManager::loadedStreams.push_back({ BASS_StreamCreateFile(false, path.first.c_str(), 0, 0, 0),path.second });
-	    if (streams != 0) {
-            BASS_ChannelSetLink(loadedStreams[0].handle, loadedStreams[streams].handle);
+    for (auto& path : paths) {
+        HSTREAM streamHandle = BASS_StreamCreateFile(false, path.first.c_str(), 0, 0, 0);
+        if (streamHandle) {
+            AudioManager::loadedStreams.push_back({ streamHandle, path.second });
+            if (streams != 0) {
+                BASS_ChannelSetLink(loadedStreams[0].handle, loadedStreams[streams].handle);
+            }
+            streams++;
+        } else {
+            std::cerr << "Failed to load stream: " << path.first << std::endl;
         }
-        streams += 1;
     }
 }
-void AudioManager::unloadStreams() {
-    AudioManager::StopPlayback(AudioManager::loadedStreams[0].handle);
-	for (auto& stream : AudioManager::loadedStreams) {
-		BASS_StreamFree(stream.handle);
-	}
-	AudioManager::loadedStreams.clear();
-}
-void AudioManager::pauseStreams() {
-	BASS_ChannelPause(loadedStreams[0].handle);
 
-}
-void AudioManager::playStreams() {
-    BASS_ChannelPlay(loadedStreams[0].handle,false);
-}
-void AudioManager::restartStreams() {
-    for (auto& stream : AudioManager::loadedStreams) {
-        BASS_ChannelSetPosition(stream.handle, 0, BASS_POS_BYTE);
+void AudioManager::unloadStreams() {
+    if (!loadedStreams.empty()) {
+        AudioManager::StopPlayback(AudioManager::loadedStreams[0].handle);
+        for (auto& stream : AudioManager::loadedStreams) {
+            BASS_StreamFree(stream.handle);
+        }
+        AudioManager::loadedStreams.clear();
     }
-	BASS_ChannelPlay(loadedStreams[0].handle, true);
 }
+
+void AudioManager::pauseStreams() {
+    if (!loadedStreams.empty()) {
+        BASS_ChannelPause(loadedStreams[0].handle);
+    }
+}
+
+void AudioManager::playStreams() {
+    if (!loadedStreams.empty()) {
+        BASS_ChannelPlay(loadedStreams[0].handle, false);
+    }
+}
+
+void AudioManager::restartStreams() {
+    if (!loadedStreams.empty()) {
+        for (auto& stream : loadedStreams) {
+            BASS_ChannelSetPosition(stream.handle, 0, BASS_POS_BYTE);
+        }
+        BASS_ChannelPlay(loadedStreams[0].handle, true);
+    }
+}
+
 double AudioManager::GetMusicTimePlayed(unsigned int handle) {
-	return BASS_ChannelBytes2Seconds(handle, BASS_ChannelGetPosition(handle,BASS_POS_BYTE));
+    return BASS_ChannelBytes2Seconds(handle, BASS_ChannelGetPosition(handle, BASS_POS_BYTE));
 }
+
 double AudioManager::GetMusicTimeLength(unsigned int handle) {
-	return BASS_ChannelBytes2Seconds(handle, BASS_ChannelGetLength(handle, BASS_POS_BYTE));
+    return BASS_ChannelBytes2Seconds(handle, BASS_ChannelGetLength(handle, BASS_POS_BYTE));
 }
+
 void AudioManager::SetAudioStreamVolume(unsigned int handle, float volume) {
-	BASS_ChannelSetAttribute(handle, BASS_ATTRIB_VOL, volume);
+    BASS_ChannelSetAttribute(handle, BASS_ATTRIB_VOL, volume);
 }
 
 void AudioManager::UpdateMusicStream(unsigned int handle) {
-	BASS_ChannelUpdate(handle, 0);
+    BASS_ChannelUpdate(handle, 0);
 }
 
 void AudioManager::BeginPlayback(unsigned int handle) {
-	BASS_ChannelStart(handle);
+    BASS_ChannelStart(handle);
 }
+
 void AudioManager::StopPlayback(unsigned int handle) {
-	BASS_ChannelStop(handle);
+    BASS_ChannelStop(handle);
 }
+
 void AudioManager::loadSample(const std::string& path, const std::string& name) {
-    unsigned int sample = BASS_SampleLoad(false, path.c_str(), 0, 0, 1, 0);
+    HSAMPLE sample = BASS_SampleLoad(false, path.c_str(), 0, 0, 1, 0);
     if (sample) {
         samples[name] = sample;
     } else {
