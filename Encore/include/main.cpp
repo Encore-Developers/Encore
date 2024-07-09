63f3bfbcebb60f791f02c3be097d69fde76236e8
3drosalia <theredrockruby@gmail.com>
2024-07-09 01:31:27 -0400

    lua scripts

--- Encore/include/main.cpp
+++ Encore/include/main.cpp
@@ -2,6 +2,16 @@
 // Created by marie on 08/07/2024.
 //
 
+#define SOL_ALL_SAFETIES_ON 1
+#define SOL_USE_LUA_HPP 1
+#include <sol/sol.hpp>
+
+
 int main(int argc, char** argv) {
+	sol::state lua;
+
+	lua.open_libraries(sol::lib::base);
+	lua.script("print('peeseburger')");
 	return 0;
 }
+
