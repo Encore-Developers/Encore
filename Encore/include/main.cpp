3edc2552325c8b6e7ad0d6d56b2bb0cf380bf3af
3drosalia <theredrockruby@gmail.com>
2024-07-13 03:32:37 -0400

    commit raylib-lua interop example

--- Encore/include/main.cpp
+++ Encore/include/main.cpp
@@ -5,13 +5,42 @@
 #define SOL_ALL_SAFETIES_ON 1
 #define SOL_USE_LUA_HPP 1
 #include <sol/sol.hpp>
-
+#include <raylib.h>
 
 int main(int argc, char *argv[]) {
 	sol::state lua;
 
 	lua.open_libraries(sol::lib::base);
-	lua.script("print('peeseburger')");
+
+
+	InitWindow(800,450, "encore");
+	SetTargetFPS(60);
+#ifdef _DEBUG
+	lua.script_file("scripts/testing.lua");
+
+#else
+	lua.script_file("scripts/testing.lua");
+#endif
+
+
+	std::string scriptString = "";
+	while (!WindowShouldClose()) {
+		if (IsKeyPressed(KEY_F5)) {
+			lua.script_file("scripts/testing.lua");
+		}
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+
+		scriptString = lua["ScriptString"];
+		DrawText("Encore is running!", 190, 200, 20, LIGHTGRAY);
+		DrawText(scriptString.c_str(), 190, 225, 20, LIGHTGRAY);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 	return 0;
 }
 
---
+++ Encore/scripts/testing.lua
@@ -0,0 +1,2 @@
+print("This is Encore running a script!")
+ScriptString = "gee i hope this doesnt crash"
