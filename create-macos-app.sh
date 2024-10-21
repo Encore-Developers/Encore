2f332b623dadd8ddc091b4d1f613430dd078504c
3drosalia <theredrockruby@gmail.com>
2024-10-21 02:29:14 -0400

    Update create-macos-app.sh

--- create-macos-app.sh
+++ create-macos-app.sh
@@ -29,7 +29,8 @@ codesign --force --deep -s - out/Encore.app
 cp -r build_macos/Encore/Songs out/Songs
 
 # -- !! TEMPORARY INDEV HACK !! --
-cp build_macos/Encore/players.json out/players.json
+# not anymore, but keeping just in case
+# cp build_macos/Encore/players.json out/players.json
 # -- !! TEMPORARY INDEV HACK !! --
 
 echo "Copy the Encore app and the Songs folder to a folder on your computer." > out/Copy_to_your_computer!.txt
