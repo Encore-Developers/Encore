c69a4189bfaabfadcd6843999f4c54b1caf5876d
maria <theredrockruby@gmail.com>
2026-06-29 19:19:09 -0400

    slightly better BASS error logging

--- Encore/src/song/audio.cpp
+++ Encore/src/song/audio.cpp
@@ -10,38 +10,86 @@
 
 #include "util/enclog.h"
 
-// Error checking macro
-#define CHECK_BASS_ERROR()                                                               \
-    {                                                                                    \
-        int errorCode = BASS_ErrorGetCode();                                             \
-        if (errorCode != BASS_OK) {                                                      \
-            Encore::Log::Error("BASS error {} at line {}", errorCode, __LINE__);           \
-                                                                            \
-            return false;                                                                \
-        }                                                                                \
+std::string BASSErrNoToString(int err) {
+    switch (err) {
+        case -1: return "BASS_ERROR_UNKNOWN";
+        case 0:  return "BASS_OK";
+        case 1:  return "BASS_ERROR_MEM";
+        case 2:	 return "BASS_ERROR_FILEOPEN";
+        case 3:	 return "BASS_ERROR_DRIVER";
+        case 4:	 return "BASS_ERROR_BUFLOST";
+        case 5:	 return "BASS_ERROR_HANDLE";
+        case 6:	 return "BASS_ERROR_FORMAT";
+        case 7:	 return "BASS_ERROR_POSITION";
+        case 8:	 return "BASS_ERROR_INIT";
+        case 9:	 return "BASS_ERROR_START";
+        case 10: return "BASS_ERROR_SSL";
+        case 11: return "BASS_ERROR_REINIT";
+        case 13: return "BASS_ERROR_TRACK";
+        case 14: return "BASS_ERROR_ALREADY";
+        case 17: return "BASS_ERROR_NOTAUDIO";
+        case 18: return "BASS_ERROR_NOCHAN";
+        case 19: return "BASS_ERROR_ILLTYPE";
+        case 20: return "BASS_ERROR_ILLPARAM";
+        case 21: return "BASS_ERROR_NO3D";
+        case 22: return "BASS_ERROR_NOEAX";
+        case 23: return "BASS_ERROR_DEVICE";
+        case 24: return "BASS_ERROR_NOPLAY";
+        case 25: return "BASS_ERROR_FREQ";
+        case 27: return "BASS_ERROR_NOTFILE";
+        case 29: return "BASS_ERROR_NOHW";
+        case 31: return "BASS_ERROR_EMPTY";
+        case 32: return "BASS_ERROR_NONET";
+        case 33: return "BASS_ERROR_CREATE";
+        case 34: return "BASS_ERROR_NOFX";
+        case 37: return "BASS_ERROR_NOTAVAIL";
+        case 38: return "BASS_ERROR_DECODE";
+        case 39: return "BASS_ERROR_DX";
+        case 40: return "BASS_ERROR_TIMEOUT";
+        case 41: return "BASS_ERROR_FILEFORM";
+        case 42: return "BASS_ERROR_SPEAKER";
+        case 43: return "BASS_ERROR_VERSION";
+        case 44: return "BASS_ERROR_CODEC";
+        case 45: return "BASS_ERROR_ENDED";
+        case 46: return "BASS_ERROR_BUSY";
+        case 47: return "BASS_ERROR_UNSTREAMABLE";
+        case 48: return "BASS_ERROR_PROTOCOL";
+        case 49: return "BASS_ERROR_DENIED";
+        case 50: return "BASS_ERROR_FREEING";
+        case 51: return "BASS_ERROR_CANCEL";
     }
+}
 
-#define CHECK_BASS_ERROR2()                                                              \
-    {                                                                                    \
-        int errorCode = BASS_ErrorGetCode();                                             \
-        if (errorCode != BASS_OK) {                                                      \
-            Encore::Log::Error("BASS error {} at line {}", errorCode, __LINE__);           \
-                                                                            \
-        }                                                                                \
-    }
+// Error checking macro
+#define CHECK_BASS_ERROR_FMT(what, ...)                                                              \
+                                                                                        \
+    int errorCode = BASS_ErrorGetCode();                                             \
+    if (errorCode != BASS_OK) {                                                      \
+        Encore::Log::Error("BASS error {} in audio.cpp func {}, line {}", BASSErrNoToString(errorCode), __FUNCTION__, __LINE__);           \
+        Encore::Log::Error(what, std::make_format_args(__VA_ARGS__));                                                                 \
+    }                                                                                \
+
+#define CHECK_BASS_ERROR(what)                                                              \
+\
+int errorCode = BASS_ErrorGetCode();                                             \
+if (errorCode != BASS_OK) {                                                      \
+Encore::Log::Error("BASS error {} in audio.cpp func {}, line {}", BASSErrNoToString(errorCode), __FUNCTION__, __LINE__);           \
+Encore::Log::Error(what);                                                                 \
+}                                                                                \
+
 
 bool Encore::AudioManager::Init() {
 #ifdef WIN32
     if (!BASS_Init(-1, 44100, 0, 0, NULL)) {
 
-        CHECK_BASS_ERROR();
+        CHECK_BASS_ERROR("Couldn't initialize BASS");
         return false;
     }
     BASS_PluginLoad("bassopus", 0);
-    CHECK_BASS_ERROR();
+    CHECK_BASS_ERROR("Couldn't initialize BASSOPUS");
 #else
     if (!BASS_Init(-1, 44100, 0, 0, NULL)) {
-        CHECK_BASS_ERROR();
+        CHECK_BASS_ERROR("Couldn't initialize BASS");
         return false;
     }
 #ifdef __APPLE__
@@ -49,8 +97,9 @@ bool Encore::AudioManager::Init() {
 #else
     BASS_PluginLoad("libbassopus.so", 0);
 #endif
-    CHECK_BASS_ERROR();
+    CHECK_BASS_ERROR("Couldn't initialize BASSOPUS");
 #endif
+    if (errorCode != BASS_OK) {return false;}
     Log::Debug("BASS Initialized");
     return true;
 }
@@ -81,8 +130,14 @@ void Encore::AudioManager::loadStreams(std::vector<std::pair<std::filesystem::pa
             BASS_ChannelSetAttribute(loadedStreams[streams].handle, BASS_ATTRIB_FREQ, rate*songSpeed*debugSpeed);
             streams++;
         } else {
-            CHECK_BASS_ERROR2();
-            std::cerr << "Failed to load stream: " << path.first << std::endl;
+            std::string pString = path.first.string();
+            std::string what = "Failed to load stream: {}";
+            // i hate you
+            int errorCode = BASS_ErrorGetCode();
+            if (errorCode != BASS_OK) {
+                Log::Error("BASS error {} in audio.cpp func {}, line {}", BASSErrNoToString(errorCode), __FUNCTION__, __LINE__);
+                Log::Error(what, pString);
+            };
         }
     }
 }
@@ -155,7 +210,7 @@ void Encore::AudioManager::UpdateAudioStreamVolumes() {
     for (const auto &stream : loadedStreams) {
         BASS_ChannelSetAttribute(stream.handle, BASS_ATTRIB_VOL, stream.volume);
     };
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to update stream volume");
 }
 
 Encore::AudioManager::AudioStream *
@@ -177,7 +232,7 @@ double Encore::AudioManager::GetMusicTimePlayed() const {
         loadedStreams[0].handle,
         BASS_ChannelGetPosition(loadedStreams[0].handle, BASS_POS_BYTE)
     ) / songSpeed;
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to get music time played");
 }
 
 double Encore::AudioManager::GetMusicTimeLength() const {
@@ -188,7 +243,7 @@ double Encore::AudioManager::GetMusicTimeLength() const {
         loadedStreams[0].handle,
         BASS_ChannelGetLength(loadedStreams[0].handle, BASS_POS_BYTE)
     ) / songSpeed;
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to music length");
 }
 
 void Encore::AudioManager::SetAudioStreamVolume(Stems stem, float volume) {
@@ -196,17 +251,17 @@ void Encore::AudioManager::SetAudioStreamVolume(Stems stem, float volume) {
         if (stem == stream.instrument)
             stream.volume = volume;
     }
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to set stream volume");
 }
 
 void Encore::AudioManager::UpdateMusicStream(unsigned int handle) {
     BASS_ChannelUpdate(handle, 0);
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to update music stream");
 }
 
 void Encore::AudioManager::BeginPlayback(unsigned int handle) {
     BASS_ChannelStart(handle);
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to begin playback");
 }
 
 
@@ -216,34 +271,42 @@ void Encore::AudioManager::StartEffect(AudioStream* stream) {
     BASS_FXSetParameters(BASS_FX_DX8_FLANGER, &effect);
     if (stream)
         stream->fxhandle = BASS_ChannelSetFX(stream->handle, BASS_FX_DX8_FLANGER, 0);
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to start channel effects");
 }
 
 void Encore::AudioManager::StopEffect(AudioStream* stream) {
     if (stream)
         BASS_ChannelRemoveFX(stream->handle, stream->fxhandle);
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to stop channel effects");
 }
 
 void Encore::AudioManager::StopPlayback(unsigned int handle) {
     BASS_ChannelStop(handle);
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to stop playback");
 }
 
 void Encore::AudioManager::SetAudioStreamPosition(unsigned int handle, double time) {
     int positionBytes = BASS_ChannelSeconds2Bytes(handle, time);
     BASS_ChannelSetPosition(handle, positionBytes, BASS_POS_BYTE);
-    CHECK_BASS_ERROR2();
+    CHECK_BASS_ERROR("Failed to set audio stream position");
 }
 
 
 void Encore::AudioManager::playSample(unsigned long sample, float volume) {
+    // see no evil, hear no evil, speak no evil
     HCHANNEL channel = BASS_SampleGetChannel(sample, false);
-    CHECK_BASS_ERROR2();
+    {
+        CHECK_BASS_ERROR("Failed to get SFX channel");
+    }
     BASS_ChannelSetAttribute(channel, BASS_ATTRIB_VOL, volume);
-    CHECK_BASS_ERROR2();
+    {
+
+        CHECK_BASS_ERROR("Failed to set SFX channel volume");
+    }
     BASS_ChannelPlay(channel, true);
-    CHECK_BASS_ERROR2();
+    {
+        CHECK_BASS_ERROR("Failed to play SFX channel");
+    }
 }
 
 unsigned long Encore::AudioManager::loadSample(bool mem, void *file, size_t length) {
